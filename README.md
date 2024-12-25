a015fe89c1f7a96db7f13428c959233999ae7349
VimalKannan31 <kvimal312003@gmail.com>
2024-12-25 13:00:47 +0530

    featur: app structure

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+# Movie-api
+provide movie api reactjs app
+
 # NodeJs
 # Nodejs
+###important command
+
+1.`npm install`
+2.`npm run server`
+3.`git init`
+4.`git add`
+5.`git commit -m "message"`
+6.`git push`
--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// node
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res) =>{
+    res.json({name : 'Vimal'})
+})
+
+app.listen(port,(req,res) =>{
+    console.log(`${port} is Running....`);
+    
+})
